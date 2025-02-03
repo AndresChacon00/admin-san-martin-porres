6d842a8186c21ba0af515e419d0bc871c85edfbc
AndresChacon00 <andresroberto.c@gmail.com>
2025-02-03 12:42:45 -0400

    creacion de tabla de prueba

---
+++ app/api/controllers/estudiantesController.ts
@@ -0,0 +1,32 @@
+import db from '../db';
+import { estudiantes } from '../tables/estudiantesSchema';
+
+export const getEstudiantes = async () => {
+  try {
+    const estudiantesList = await db
+      .select()
+      .from(estudiantes)
+      .orderBy(estudiantes.id);
+
+    return estudiantesList;
+  } catch (error) {
+    console.error('Error al obtener a los estudiantes: ', error);
+  }
+};
+
+export const addEstudiante = async (
+  name: string,
+  age: number,
+  email: string,
+) => {
+  try {
+    const newEstudiante = await db
+      .insert(estudiantes)
+      .values({ name, age, email })
+      .returning();
+
+    return newEstudiante[0];
+  } catch (error) {
+    console.error('Error al añadir un estudiante: ', error);
+  }
+};
---
+++ app/api/initDB.ts
@@ -0,0 +1,20 @@
+import db from './db';
+import { estudiantes } from './tables/estudiantesSchema';
+
+await db.transaction(async (tx) => {
+  const estudiantesEjemplo = [
+    {
+      name: 'Andres',
+      age: 20,
+      email: 'andresroberto.c@gmail.com',
+    },
+  ];
+
+  for (const estudiante of estudiantesEjemplo) {
+    try {
+      await tx.insert(estudiantes).values(estudiante).onConflictDoNothing();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+});
