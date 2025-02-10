c47688c8f7c8b8d9e7d56c94b9a7594a5963c050
AndresChacon00 <andresroberto.c@gmail.com>
2025-02-10 11:05:58 -0400

    services for estudiantes get create update and delete

---
+++ app/api/services/estudiantes.server.ts
@@ -0,0 +1,57 @@
+import { eq } from 'drizzle-orm';
+import db from '../db';
+import { estudiantes } from '../tables/estudiantes';
+import { EstudianteInsert, EstudianteUpdate } from '~/types/estudiantes.types';
+
+/**
+ * Gets a single student
+ * @author Andrés
+ * @param id
+ * @throws if the student could not be selected
+ */
+export async function getSingleEstudiante(id: number) {
+  const estudiante = await db
+    .select()
+    .from(estudiantes)
+    .where(eq(estudiantes.id, id));
+
+  return estudiante[0];
+}
+
+/**
+ * Inserts a new student into de database
+ * @author Andrés
+ * @param data
+ * @throws if the student could not be inserted
+ */
+export async function createEstudiante(data: EstudianteInsert) {
+  const newEstudiante = await db.insert(estudiantes).values(data).returning();
+  return newEstudiante[0];
+}
+
+/**
+ * Updates a student in the database by ID
+ * @author Andrés
+ * @param id
+ * @param data
+ * @throws if the student could not be updated
+ */
+export async function updateEstudianteInDb(id: number, data: EstudianteUpdate) {
+  const updatedEstudiante = await db
+    .update(estudiantes)
+    .set(data)
+    .where(eq(estudiantes.id, id))
+    .returning();
+
+  return updatedEstudiante[0];
+}
+
+/**
+ * Deletes a student in the database by ID
+ * @author Andrés
+ * @param id
+ * @throws if the student could not be deleted
+ */
+export async function deleteEstudianteInDb(id: number) {
+  await db.delete(estudiantes).where(eq(estudiantes.id, id));
+}
