9ac46cb79bbb0a21309ee20824757a06a96f225b
Roberth <roberthgraffegraffe@gmail.com>
2025-02-27 23:57:57 -0400

    añadir cursos a periodos

---
+++ app/types/cursosPeriodo.types.ts
@@ -0,0 +1,17 @@
+import { cursosPeriodo } from '~/api/tables/cursosPeriodo';
+
+export type CursoPeriodoInsert = typeof cursosPeriodo.$inferInsert;
+
+export type CursoPeriodoUpdate = Partial<CursoPeriodoInsert>;
+
+export type CursoPeriodo = typeof cursosPeriodo.$inferSelect;
+
+export type CursoEnPeriodo = {
+  idPeriodo: number;
+  codigo: string;
+  nombreCurso: string;
+  descripcion: string | null;
+  horario: string | null;
+  estado: number;
+  precioTotal: number | null;
+};
