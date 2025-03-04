6976b4f0367d96b0bd20b96c8b36e66f780e0d9c
gabrielmj23 <gabrielmj2312@gmail.com>
2025-03-04 10:43:07 -0400

    feat(empleados): delete empleados from table

--- app/routes/profesores._index.tsx
+++ app/routes/profesores._index.tsx
@@ -1,10 +1,18 @@
-import { Link, MetaFunction, useLoaderData } from '@remix-run/react';
+import {
+  Link,
+  MetaFunction,
+  useFetcher,
+  useLoaderData,
+} from '@remix-run/react';
 import { writeFile } from 'xlsx';
-import { getProfesores } from '~/api/controllers/profesores';
+import { deleteProfesor, getProfesores } from '~/api/controllers/profesores';
 import { empleadoColumns } from '~/components/columns/empleados-columns';
 import { Button } from '~/components/ui/button';
-import { DataTable } from '~/components/ui/data-table';
+import { DataTable } from '~/components/data-tables/empleados-data-table';
 import { exportEmpleados } from '~/lib/exporters';
+import { ActionFunctionArgs } from '@remix-run/node';
+import { useEffect } from 'react';
+import { toast } from 'sonner';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Profesores | San Martín de Porres' }];
@@ -15,14 +23,49 @@ export async function loader() {
   return data;
 }
 
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  let _action = formData.get('_action');
+  if (!_action) {
+    return {
+      type: 'error',
+      message: 'Suministre una acción',
+    } as const;
+  }
+
+  _action = String(_action);
+  if (_action === 'delete') {
+    const profesorId = formData.get('profesorId');
+    if (!profesorId) {
+      return {
+        type: 'error',
+        message: 'Especifique el empleado a eliminar',
+      } as const;
+    }
+    return deleteProfesor(Number(profesorId));
+  }
+}
+
 export default function ProfesoresPage() {
   const data = useLoaderData<typeof loader>();
+  const fetcher = useFetcher<typeof action>();
 
   const handleExport = () => {
     if ('type' in data) return;
     const workbook = exportEmpleados(data);
     writeFile(workbook, 'profesores.xlsx', { compression: true });
   };
+
+  useEffect(() => {
+    if (fetcher.state === 'idle' && fetcher.data !== undefined) {
+      if (fetcher.data.type === 'success') {
+        toast.success('Profesor eliminado del sistema');
+      } else if (fetcher.data.type === 'error') {
+        toast.error('Ocurrió un error eliminando el profesor');
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fetcher.state, fetcher.data]);
 
   return (
     <>
@@ -44,7 +87,16 @@ export default function ProfesoresPage() {
             <p>Ocurrió un error cargando los datos</p>
           )}
           {!('type' in data) && (
-            <DataTable columns={empleadoColumns} data={data} />
+            <DataTable
+              columns={empleadoColumns}
+              data={data}
+              deleteEmployee={(id) =>
+                fetcher.submit(
+                  { _action: 'delete', profesorId: id },
+                  { method: 'POST', action: '/profesores' },
+                )
+              }
+            />
           )}
         </div>
       </main>
