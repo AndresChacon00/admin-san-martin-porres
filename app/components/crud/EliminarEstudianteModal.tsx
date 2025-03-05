a75c20e3d803f9130ca8ec29bca021a31c051c31
AndresChacon00 <andresroberto.c@gmail.com>
2025-03-04 22:52:52 -0400

    Modal eliminar Estudiante

---
+++ app/components/crud/EliminarEstudianteModal.tsx
@@ -0,0 +1,70 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogTrigger,
+} from '~/components/ui/dialog';
+import { Button } from '~/components/ui/button';
+import { useFetcher } from '@remix-run/react';
+import { useState } from 'react';
+
+interface FormValues {
+  id: number;
+  nombre: string;
+  apellido: string;
+  cedula: string;
+  sexo: string;
+  fechaNacimiento: Date | null;
+  edad: number;
+  religion: string;
+  telefono: string;
+  correo: string;
+  direccion: string;
+  ultimoAñoCursado: string;
+}
+
+interface EliminarEstudianteModalProps {
+  estudiante: FormValues;
+}
+
+export function EliminarEstudianteModal({
+  estudiante,
+}: EliminarEstudianteModalProps) {
+  const fetcher = useFetcher();
+  const [values, setValues] = useState<FormValues>(() => ({
+    ...estudiante,
+  }));
+
+  function handleDelete() {
+    const formData = new FormData();
+    formData.append('actionType', 'eliminar');
+    formData.append('id', values.id.toString());
+    fetcher.submit(formData, { method: 'post' });
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant='destructive'>Eliminar</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Eliminar estudiante</DialogTitle>
+          <DialogDescription>
+            ¿Estás seguro que deseas eliminar a {values.nombre}{' '}
+            {values.apellido}?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button variant='ghost'>Cancelar</Button>
+          <Button variant='destructive' onClick={handleDelete}>
+            Sí, eliminar
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
