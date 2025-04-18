ebcdb7e4a9ae82ed7561138a07e9a341715577eb
gabrielmj23 <gabrielmj2312@gmail.com>
2025-04-18 13:05:19 -0400

    fix(pagos-empleados): agregar totales a la tabla de nomina

---
+++ drizzle/0002_bumpy_cargill.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `pagos` ADD `total_asignaciones` real NOT NULL;--> statement-breakpoint
+ALTER TABLE `pagos` ADD `total_adicional` real NOT NULL;--> statement-breakpoint
+ALTER TABLE `pagos` ADD `total_deducciones` real NOT NULL;--> statement-breakpoint
+ALTER TABLE `pagos` ADD `total_nomina` real NOT NULL;
