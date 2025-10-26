c1d3b4ff5c795c767e961bfa6beca5f550e05fc4
gabrielmj23 <gabrielmj2312@gmail.com>
2025-10-26 19:45:57 -0400

    fix fechas de las tablas

--- app/components/columns/periodos-columns.tsx
+++ app/components/columns/periodos-columns.tsx
@@ -1,21 +1,26 @@
+import { ColumnDef } from '@tanstack/react-table';
 
-import { ColumnDef } from "@tanstack/react-table";
-
-import { Periodo } from "~/types/periodos.types";
+import { Periodo } from '~/types/periodos.types';
 
 export const periodosColumns: ColumnDef<Periodo>[] = [
   {
-    accessorKey: "idPeriodo",
-    header: "ID Periodo",
+    accessorKey: 'idPeriodo',
+    header: 'ID Periodo',
   },
   {
-    accessorKey: "fechaInicio",
-    header: "Fecha de Inicio",
-    accessorFn: (row) => new Date(row.fechaInicio).toLocaleDateString(),
+    accessorKey: 'fechaInicio',
+    header: 'Fecha de Inicio',
+    accessorFn: (row) =>
+      new Date(row.fechaInicio).toLocaleDateString('es-VE', {
+        timeZone: 'GMT',
+      }),
   },
   {
-    accessorKey: "fechaFin",
-    header: "Fecha de Fin",
-    accessorFn: (row) => new Date(row.fechaFin).toLocaleDateString(),
+    accessorKey: 'fechaFin',
+    header: 'Fecha de Fin',
+    accessorFn: (row) =>
+      new Date(row.fechaFin).toLocaleDateString('es-VE', {
+        timeZone: 'GMT',
+      }),
   },
 ];
